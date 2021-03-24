53b6d2f467a336ed0468a00bc302683d08ce3c96
Kotora-source <61540072+Kotora-source@users.noreply.github.com>
2021-03-24 09:46:09 +0100

    Delete dictator.js

--- src/commands/dictator.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'dictator',
-    description : 'Send a cheer to our dictator',
-    execute(message) {
-        message.channel.send(`Gloire à notre dictatrice !`);
-    }
-};
